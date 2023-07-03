d38ee4683af8f18e088c0b7ad4f7395020ecd158
yc97463 <hi@imych.one>
2023-07-03 17:31:50 +0800

    set lang to en-US

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'hi@imych.one',
   link: 'https://blog.imych.one',
   description: '願以照片攝下生活，一場人生中的旅行，從此展開。',
-  lang: 'zh-TW', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Taipei', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: ['Space Grotesk', 'sans-serif'], // ['sans-serif', 'serif']
